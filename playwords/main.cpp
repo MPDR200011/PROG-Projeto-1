0e03603320b57968e754ca1d7e62a24c926de515
Moisés Rocha <moisesrocha99@gmail.com>
2018-04-13 21:30:03 +0100

    done

--- playwords/main.cpp
+++ playwords/main.cpp
@@ -24,42 +24,38 @@ int main() {
 	} while (!validFile);
 
 	//Itens do menu
-
- cout << "                                          __ " << endl;
- cout << " _____ _            _ _ _           _    |  |" << endl;
- cout << "|  _  | |___ _ _   | | | |___ ___ _| |___|  |" << endl;
- cout << "|   __| | .'| | |  | | | | . |  _| . |_ -|__|" << endl;
- cout << "|__|  |_|__,|_  |  |_____|___|_| |___|___|__|" << endl;
- cout << "            |___|                            " << endl;
- cout << " ___________________________________________ " << endl;
- cout << "//|     **   Bem vindo a Play Words!   **     |\\\\" << endl;
- cout << "\\\\|             Escolha uma opcao             |//" << endl;
- cout << " \\\\___________________________________________// " << endl;
- cout << "   |//                 1                   \\\\|   " << endl;
- cout << "   ||    Palavra pertence ao dicionario?    ||   " << endl;
- cout << "   |\\\\                                     //|   " << endl;
- cout << "   |//                 2                   \\\\|   " << endl;
- cout << "   ||           Adivinhar palavra           ||   " << endl;
- cout << "   |\\\\                                     //|   " << endl;
- cout << "   |//                 3                   \\\\|   " << endl;
- cout << "   ||  Palavras existentes com letras dadas ||   " << endl;
- cout << "   |\\\\                                     //|   " << endl;
- cout << "   |//                 4                   \\\\|   " << endl;
- cout << "   ||Construir palavra com letras aleatorias||   " << endl;
- cout << "   |\\\\                                     //|   " << endl;
- cout << "   |//                 5                   \\\\|   " << endl;
- cout << "   ||   Palavras com characteres Wildcard   ||   " << endl;
- cout << "   |\\\\_____________________________________//|   " << endl;
-
-
 	bool anotherGame = false;
 	do {
+
+		cout << "                                          __ " << endl;
+		cout << " _____ _            _ _ _           _    |  |" << endl;
+		cout << "|  _  | |___ _ _   | | | |___ ___ _| |___|  |" << endl;
+		cout << "|   __| | .'| | |  | | | | . |  _| . |_ -|__|" << endl;
+		cout << "|__|  |_|__,|_  |  |_____|___|_| |___|___|__|" << endl;
+		cout << "            |___|                            " << endl;
+		cout << " ___________________________________________ " << endl;
+		cout << "//|     **   Bem vindo a Play Words!   **     |\\\\" << endl;
+		cout << "\\\\|             Escolha uma opcao             |//" << endl;
+		cout << " \\\\___________________________________________// " << endl;
+		cout << "   |//                 1                   \\\\|   " << endl;
+		cout << "   ||    Palavra pertence ao dicionario?    ||   " << endl;
+		cout << "   |\\\\                                     //|   " << endl;
+		cout << "   |//                 2                   \\\\|   " << endl;
+		cout << "   ||           Adivinhar palavra           ||   " << endl;
+		cout << "   |\\\\                                     //|   " << endl;
+		cout << "   |//                 3                   \\\\|   " << endl;
+		cout << "   ||  Palavras existentes com letras dadas ||   " << endl;
+		cout << "   |\\\\                                     //|   " << endl;
+		cout << "   |//                 4                   \\\\|   " << endl;
+		cout << "   ||Construir palavra com letras aleatorias||   " << endl;
+		cout << "   |\\\\                                     //|   " << endl;
+		cout << "   |//                 5                   \\\\|   " << endl;
+		cout << "   ||   Palavras com characteres Wildcard   ||   " << endl;
+		cout << "   |\\\\_____________________________________//|   " << endl;
 
 		if (anotherGame) {
 			file.open(fileName);
 		}
-
-		
 
 		//Escolher jogo
 		size_t choice;
@@ -77,13 +73,13 @@ int main() {
 
 		switch (choice) {
 			case (1):
-				func1();
+				func1(file);
 				break;
 			case (2):
-				func2();
+				func2(file);
 				break;
 			case (3):
-				func3();
+				func3(file);
 				break;
 			case (4):
 				randomNLetters(file);
@@ -124,104 +120,4 @@ int main() {
 	}
 
 	return 0;
-}
-// funcoes side
-bool isMember_word(string word, vector<string> list) {
-
-	for (unsigned i = 0; i < word.length(); i++) {
-		word[i] = toupper(word[i]);
-	}
-
-	for (unsigned i = 0; i < list.size(); i++) {
-		if (word == list[i])
-			return true;
-	}
-	return false;
-}
-
-string scramble(string word) {
-	string scramb = word;
-	for (unsigned i = 0; i < scramb.length(); i++) {
-		unsigned j = rand() % scramb.length();
-		swap(scramb[i], scramb[j]);
-	}
-	return scramb;
-}
-
-bool char_pertence(char ch, string word) {
-	for (unsigned i = 0; i < word.length(); i++) {
-		if (ch == word[i]) {
-			return true;
-		}
-	}
-	return false;
-}
-
-
-//funcoes main
-
-void func1(vector<string> wordList) {			// Verificar se palavra pertence ao dicionario
-	string word;
-	cout << "Insira a palavra que quer pesquisar: "; cin >> word;
-	cout << '\n';
-
-	if (isMember_word(word, wordList)) { cout << "A palavra " << word << " pertence ao dicionario.\n"; }
-	else { cout << "A palavra " << word << " nao pertence ao dicionario.\n"; }
-}
-
-void func2(vector<string> wordList) {			// Adivinhar uma palavra baralhada
-	int wordIndex = rand() % wordList.size();
-	string word = wordList[wordIndex];
-	string scrWord = scramble(word);
-	string guessWord;
-	cout << scrWord << "\n\n";
-	for (int v = 3; v >= 0; v--) {
-		if (v == 0) {
-			cout << '\n' << "Perdeu! A resposta era " << word << "!\n";
-			break;
-		}
-		cin >> guessWord;
-		if (word == guessWord) {
-			cout << '\n' << "Acertou! Parabens!\n";
-			break;
-		}
-	}
-}
-
-void func3(vector<string> wordList) {			// visualizar palavras com um set de letras
-	string letras_in;
-	string letras_mid;
-	string letras;
-	cout << "Insira agora as letras que fazem parte do conjunto:\n";
-	cin.ignore();
-	getline(cin, letras_in);
-	size_t compin = letras_in.length();
-	size_t comp = letras.length();
-
-	for (unsigned i = 0; i < compin; i++) {                        // remove espacos e qlqr simbolo e poe tudo em maiusculas
-		if ((letras_in[i] > 64 && letras_in[i] < 91) || (letras_in[i] > 96 && letras_in[i] < 123)) {
-			letras = letras + char(toupper(letras_in[i]));
-		}
-	}
-
-	cout << letras << '\t';                                  // mostra o set de letras
-
-	cout << "\n\n" << "As palavras as quais pertencem todas as letras do conjunto sao:\n";
-	bool pertence = false;
-	for (unsigned i = 0; i < wordList.size(); i++) {
-		pertence = true;
-		for (unsigned j = 0; j < letras.length(); j++) {
-			if (sizeof(wordList[i]) > letras.length()) {
-				pertence = false;
-				break;
-			}
-			if (!char_pertence(letras[j], wordList[i])) {
-				pertence = false;
-				break;
-			}
-		}
-		if (pertence == true) {
-			cout << wordList[i] << '\n';
-		}
-	}
 }
--- playwords/router.h
+++ playwords/router.h
@@ -2,7 +2,14 @@
 
 #include <fstream>
 
-int playwords();
+//Exercicio 1
+void func1(std::ifstream &file);
+
+//Exercicio 2
+void func2(std::ifstream &file);
+
+//Exercicio 3
+void func3(std::ifstream &file);
 
 //Exercicio 4
 void randomNLetters(std::ifstream &wordListFile);
